b2a8d00f534e2da76f73a86c557cf42bacb65f4c
Rahul Yadav <torahulyadav@gmail.com>
2016-06-07 17:20:31 +0530

    update strings

--- app/src/main/java/com/game/tictactoe/example/GameActivity.java
+++ app/src/main/java/com/game/tictactoe/example/GameActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -56,13 +55,6 @@ public class GameActivity extends Activity implements View.OnClickListener, Game
         status.setText(" Level : ".concat(game.getLevel().name()));
 
         startButton.performClick();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.normal, menu);
-        return true;
     }
 
     public void makeMove(View v) {
